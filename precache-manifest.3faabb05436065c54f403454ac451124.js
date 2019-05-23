64e6ebe94703e2dac454a9501ad2d86dc30ce2b5
Dom Geargeoura <dgeargeoura@H7233.ad.hugeinc.com>
2019-05-23 10:23:55 +0100

    Updates

---
+++ precache-manifest.3faabb05436065c54f403454ac451124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87f0592b9160a71d7137a53ace22f64",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "7709775830fdad0848b9",
+    "url": "/pokemon/static/css/main.3ba1e4c6.chunk.css"
+  },
+  {
+    "revision": "d25b18cc0871fa11bfb4",
+    "url": "/pokemon/static/js/2.0612f017.chunk.js"
+  },
+  {
+    "revision": "7709775830fdad0848b9",
+    "url": "/pokemon/static/js/main.9cdc4171.chunk.js"
+  },
+  {
+    "revision": "b04917ec47d1c084d1e9",
+    "url": "/pokemon/static/js/runtime~main.0909cbae.js"
+  },
+  {
+    "revision": "3cfd5e85cd2597eb00d7f5b0139d3b6d",
+    "url": "/pokemon/static/media/pokemon-logo.3cfd5e85.svg"
+  }
+]);
